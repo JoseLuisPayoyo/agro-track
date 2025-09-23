c8712479a32fae786d357f0a9d4fb182c58412b3
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-09-23 18:56:36 +0200

    actualizada la entidad de campaña

--- agro-track-backend/src/main/java/com/payoyo/agro_track/models/Campaign.java
+++ agro-track-backend/src/main/java/com/payoyo/agro_track/models/Campaign.java
@@ -1,14 +1,20 @@
 package com.payoyo.agro_track.models;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -32,14 +38,26 @@ public class Campaign {
     private String name;
 
     @Column(nullable = false)
-    @NotBlank(message = "La fecha de inicio es obligatoria")
+    @NotNull(message = "La fecha de inicio es obligatoria")
     private LocalDate startDate;
 
     private LocalDate endDate;
 
     @Column(nullable = false ,length = 200)
-    @NotNull(message = "La tarea de la campaña es obligatoria")
+    @NotNull(message = "La tarea principal de la campaña es obligatoria")
     private String mainTask;
 
-    
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "farm_id", nullable = false)
+    @NotNull(message = "La finca es obligatoria")
+    private Farm farm;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "crew_id", nullable = false)
+    @NotNull(message = "La cuadrilla es obligatoria")
+    private Crew crew;
+
+    @OneToMany(mappedBy = "campaign", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = false)
+    private List<WorkPart> workParts = new ArrayList<>();
+
 }

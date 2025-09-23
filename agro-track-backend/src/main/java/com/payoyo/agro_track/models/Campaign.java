26896f099adbfda1957a7b69d54991fe6e3475a3
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-09-23 18:49:11 +0200

    creada la entidad campaña

--- agro-track-backend/src/main/java/com/payoyo/agro_track/models/Campaign.java
+++ agro-track-backend/src/main/java/com/payoyo/agro_track/models/Campaign.java
@@ -1,5 +1,45 @@
 package com.payoyo.agro_track.models;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "campaigns")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Campaign {
+    
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, length = 80)
+    @NotNull(message = "El nombre de la camapa es obligatorio")
+    private String name;
+
+    @Column(nullable = false)
+    @NotBlank(message = "La fecha de inicio es obligatoria")
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    @Column(nullable = false ,length = 200)
+    @NotNull(message = "La tarea de la campaña es obligatoria")
+    private String mainTask;
+
     
 }

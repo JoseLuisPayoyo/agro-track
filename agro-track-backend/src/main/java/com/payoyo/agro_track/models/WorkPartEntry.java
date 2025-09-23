368bdf0c6bc3dfa5df9b669bdd30094e967d71fc
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-09-23 19:32:59 +0200

    creada la entidad workpartentry

--- agro-track-backend/src/main/java/com/payoyo/agro_track/models/WorkPartEntry.java
+++ agro-track-backend/src/main/java/com/payoyo/agro_track/models/WorkPartEntry.java
@@ -1,5 +1,51 @@
 package com.payoyo.agro_track.models;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "work_parts_entries")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class WorkPartEntry {
     
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "work_part_id", nullable = false)
+    private WorkPart workPart;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "employee_id", nullable = false)
+    @NotNull(message = "El empleado es obligatorio")
+    private Employee employee;
+
+    @Column(nullable = false)
+    @NotNull(message = "La horas trabajadas son obligatorias")
+    @PositiveOrZero(message = "La horas no pueden ser negativas")
+    private BigDecimal hoursWorked;
+
+    @DecimalMin(value = "0", inclusive = false)
+    private BigDecimal quantityKg;
+
 }
